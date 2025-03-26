274ad041fa280860612ce6f1831d668b909338ae
NachyZuri <yukyuel@gmail.com>
2025-03-26 18:22:12 -0500

    update 5

---
+++ ms_kajita/src/main/java/com/example/mskajita/entity/Categoria.java
@@ -0,0 +1,49 @@
+package com.example.mskajita.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Data
+public class Categoria {
+
+    public Categoria() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private LocalDateTime fechaCreacion;
+
+    public Categoria(Integer id, String nombre, String descripcion, LocalDateTime fechaCreacion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fechaCreacion = fechaCreacion;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", fechaCreacion=" + fechaCreacion +
+                '}';
+    }
+
+
+}
